1466ec77dfa6f94f0f43c1ed8941decf9e3f5966
swyx <142970592+swyx@users.noreply.github.com>
2023-09-10 14:23:24 -0500

    Update live.php

--- PlutoTV/live.php
+++ PlutoTV/live.php
@@ -1,8 +1,8 @@
 <?php
 // Obtener el valor del parámetro 'tv' en la URL
-$live = $_GET['tv'];
+$tv = $_GET['tv'];
 
-// Definir las URL de destino para cada canal
+// Definir las URL de destino para cada canal con nombres descriptivos
 $urls = array(
     'guiamx' => 'http://stitcher-ipv4.pluto.tv/v1/stitch/embed/hls/channel/5e793a7cfbdf780007f7eb75/master.m3u8?deviceType=samsung-tvplus&deviceMake=samsung&deviceModel=samsung&deviceVersion=unknown&appVersion=unknown&deviceLat=0&deviceLon=0&deviceDNT=%7BTARGETOPT%7D&deviceId=%7BPSID%7D&advertisingId=%7BPSID%7D&us_privacy=1YNY&samsung_app_domain=%7BAPP_DOMAIN%7D&samsung_app_name=%7BAPP_NAME%7D&profileLimit=&profileFloor=&embedPartner=samsung-tvplus',
     'startreck' => 'http://stitcher-ipv4.pluto.tv/v1/stitch/embed/hls/channel/5ec5761aabe4690007f6e047/master.m3u8?deviceType=samsung-tvplus&deviceMake=samsung&deviceModel=samsung&deviceVersion=unknown&appVersion=unknown&deviceLat=0&deviceLon=0&deviceDNT=%7BTARGETOPT%7D&deviceId=%7BPSID%7D&advertisingId=%7BPSID%7D&us_privacy=1YNY&samsung_app_domain=%7BAPP_DOMAIN%7D&samsung_app_name=%7BAPP_NAME%7D&profileLimit=&profileFloor=&embedPartner=samsung-tvplus',
